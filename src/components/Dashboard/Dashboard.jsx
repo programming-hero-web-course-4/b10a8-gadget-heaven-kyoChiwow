367099cca44d0d37d2c6f0594a64bd4ce27710fa
kyoChiwow <kyoayasakicsgo@gmail.com>
2024-11-04 01:53:08 +0600

    First commit

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard Page here</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page here</h1>
+        </div>
+    );
+};
+
+export default HomePage;
