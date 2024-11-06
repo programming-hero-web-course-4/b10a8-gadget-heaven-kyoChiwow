28376ceae593b764a8c3df1f5e6ed80a88ac3396
kyoChiwow <kyoayasakicsgo@gmail.com>
2024-11-06 20:01:19 +0600

    Done with the project

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,37 @@
+import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>Statistics | Gadget Heaven</title>
-                <link rel="icon" href="/assets/favicon-16x16.png" />
-            </Helmet>
-            <h1>This is stats</h1>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("gadgetsData.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const chartData = products.map((product) => ({
+    name: product.product_title,
+    price: product.price,
+  }));
+  return (
+    <div className="mt-10">
+      <Helmet>
+        <title>Statistics | Gadget Heaven</title>
+        <link rel="icon" href="/assets/favicon-16x16.png" />
+      </Helmet>
+      <ResponsiveContainer width="100%" height={600}>
+        <BarChart data={chartData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="price" fill="#8884d8"/>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Statistics;
